957d1e1126c250bd154ebac340b0dbfd9ab86f64
ChngWC <chngwc123@gmail.com>
2022-05-26 17:35:22 +0800

    Changed the background of ProfileActivity, added sign out button and function and display current user name and email at ProfileActivity

--- app/src/main/java/com/example/taskmaster/ProfileActivity.java
+++ app/src/main/java/com/example/taskmaster/ProfileActivity.java
@@ -1,14 +1,78 @@
 package com.example.taskmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+    private Button signout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        signout = (Button) findViewById(R.id.signout);
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(ProfileActivity.this, "Successfully signed out!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance("https://taskmaster-15439-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users");
+        userID = user.getUid();
+
+        final TextView emailTextView = (TextView) findViewById(R.id.emailText);
+        final TextView nameTextView = (TextView)  findViewById(R.id.nameText);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                
+                if(userProfile != null) {
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+
+                    fullName = fullName.substring(0, Math.min(fullName.length(), 15));
+                    email = email.substring(0, Math.min(email.length(), 15));
+
+
+                    nameTextView.setText(fullName + "...");
+                    emailTextView.setText(email +"...");
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Unexpected error!!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
