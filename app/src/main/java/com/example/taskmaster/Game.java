3eed58c1d350284e7bf999eb494645447d1697fc
ChngWC <chngwc123@gmail.com>
2022-06-23 10:53:50 +0800

    Added movable circle or Player

--- app/src/main/java/com/example/taskmaster/Game.java
+++ app/src/main/java/com/example/taskmaster/Game.java
@@ -3,6 +3,7 @@ package com.example.taskmaster;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -11,8 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private final Player player;
     private GameLoop gameLoop;
-    private Context context;
+   // private Context context;
 
     public Game(Context context) {
         super(context);
@@ -20,11 +22,28 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         //Get surface holder and add callback
         SurfaceHolder surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
-        this.context = context;
+        //this.context = context;
 
          gameLoop = new GameLoop(this, surfaceHolder);
 
+         player = new Player(getContext(), 2*500, 500, 30);
+
         setFocusable(true);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        //Handle different touch evens
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                player.setPosition((double)event.getX(), (double)event.getY());
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                player.setPosition((double)event.getX(), (double)event.getY());
+                return true;
+        }
+        return super.onTouchEvent(event);
     }
 
     @Override
@@ -48,12 +67,14 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         super.draw(canvas);
         drawFPS(canvas);
         drawUPS(canvas);
+
+        player.draw(canvas);
     }
 
     public void drawUPS (Canvas canvas) {
         String averageUPS = Double.toString(gameLoop.getAverageUPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.purple_500);
+        int color = ContextCompat.getColor(getContext(), R.color.purple_500);
         paint.setColor(color);
         paint.setTextSize(50);
         canvas.drawText("UPS: " + averageUPS, 100, 100, paint);
@@ -62,12 +83,13 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
     public void drawFPS (Canvas canvas) {
         String averageUPS = Double.toString(gameLoop.getAverageFPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.purple_500);
+        int color = ContextCompat.getColor(getContext(), R.color.purple_500);
         paint.setColor(color);
         paint.setTextSize(50);
         canvas.drawText("FPS: " + averageUPS, 100, 200, paint);
     }
 
     public void update() {
+        player.update();
     }
 }
